928020be5df3cd4c0347871a6d8ac7993940680c
Wesley Tokoi <tokoiwesley@gmail.com>
2021-11-19 16:11:47 +0300

    Use immutable variables

--- NoClasses/src/main/kotlin/main.kt
+++ NoClasses/src/main/kotlin/main.kt
@@ -1,3 +1,12 @@
 fun main(args: Array<String>) {
     println("Hello, World")
+    val q = Question()
+//    q = Question()
+    q.Answer = "42"
+    q.Question = ""
+}
+
+class Question {
+    var Answer: String = ""
+    val Question: String = "What is the answer to life, the universe and everything?"
 }
