b559c3facde302f96bbd5507987edd071f789087
Wesley Tokoi <tokoiwesley@gmail.com>
2021-11-20 23:18:46 +0300

    Add a string template

--- NoClasses/src/main/kotlin/main.kt
+++ NoClasses/src/main/kotlin/main.kt
@@ -3,10 +3,17 @@ fun main(args: Array<String>) {
     val q = Question()
 //    q = Question()
     q.Answer = "42"
-    q.Question = ""
+//    q.Question = ""
+    q.display()
+
+    println("The answer to ${q.Question} is ${q.Answer}")
 }
 
 class Question {
     var Answer: String = ""
     val Question: String = "What is the answer to life, the universe and everything?"
+
+    fun display() {
+        println("You said $Answer")
+    }
 }
