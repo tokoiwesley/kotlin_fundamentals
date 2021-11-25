c8a30438e2c530e506512c4ba1552042a98d4396
Wesley Tokoi <tokoiwesley@gmail.com>
2021-11-25 22:26:44 +0300

    Use a function expression

--- Functions/src/main/kotlin/dev/tokoi/Program.kt
+++ Functions/src/main/kotlin/dev/tokoi/Program.kt
@@ -1,10 +1,7 @@
 package dev.tokoi
 
 fun main(args: Array<String>) {
-    display("Hello, World!")
+    println(max(1, 2))
 }
 
-fun display(message: String): Boolean {
-    println(message)
-    return true
-}
+fun max(a: Int, b: Int): Int = if (a > b) a else b
