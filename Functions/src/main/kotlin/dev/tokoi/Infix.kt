9d03d7265dcd0cc25bd66fc52ed7d4caa5833361
Wesley Tokoi <tokoiwesley@gmail.com>
2021-11-28 23:43:31 +0300

    Create an infix function

---
+++ Functions/src/main/kotlin/dev/tokoi/Infix.kt
@@ -0,0 +1,18 @@
+package dev.tokoi
+
+fun main(args: Array<String>) {
+    val h1 = Header("H1")
+    val h2 = Header("H2")
+
+    val h3 = h1 plus h2
+
+    println(h3.Name)
+}
+
+class Header(var Name: String) {
+
+}
+
+infix fun Header.plus(other: Header): Header {
+    return Header(this.Name + other.Name)
+}
