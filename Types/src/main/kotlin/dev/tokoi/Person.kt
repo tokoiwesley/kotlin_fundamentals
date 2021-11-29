9e695af4e5a6aa6143fda2730f9a513d8093d142
Wesley Tokoi <tokoiwesley@gmail.com>
2021-11-29 22:39:32 +0300

    Define an abstract class

--- Types/src/main/kotlin/dev/tokoi/Person.kt
+++ Types/src/main/kotlin/dev/tokoi/Person.kt
@@ -1,13 +1,18 @@
 package dev.tokoi
 
-open class Person {
+abstract class Person {
     var firsName: String = ""
     var lastName: String = ""
 
     open fun getName(): String = "$firsName $lastName"
+    abstract fun getAddress(): String
 }
 
 class Student() : Person() {
+    override fun getAddress(): String {
+        return ""
+    }
+
     override fun getName(): String {
         return ""
     }
