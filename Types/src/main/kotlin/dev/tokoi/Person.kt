f551abe9116216bf62931b4ea6a051e815a9e85b
Wesley Tokoi <tokoiwesley@gmail.com>
2021-11-30 23:49:26 +0300

    Pass values up to a base constructor

--- Types/src/main/kotlin/dev/tokoi/Person.kt
+++ Types/src/main/kotlin/dev/tokoi/Person.kt
@@ -1,24 +1,22 @@
 package dev.tokoi
 
 fun main(args: Array<String>) {
-    val wesley = Student(1)
+    val wesley = Student("Wesley", "Tokoi", 1)
     print(wesley.id)
 }
 
-abstract class Person {
-    var firsName: String = ""
-    var lastName: String = ""
-
+abstract class Person(var firsName: String, var lastName: String) {
     open fun getName(): String = "$firsName $lastName"
     abstract fun getAddress(): String
 }
 
-class Student(_id: Int) : Person() {
+class Student(firsName: String, lastName: String, _id: Int) : Person(firsName, lastName) {
     val id: Int
 
     init {
         id = _id
     }
+
     override fun getAddress(): String {
         return ""
     }
