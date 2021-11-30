a6cf47abdf1c9c135a55334861675245d48a5398
Wesley Tokoi <tokoiwesley@gmail.com>
2021-12-01 00:17:49 +0300

    Copy a data class and override a parameter

--- DataClasses/src/main/kotlin/dev/tokoi/Program.kt
+++ DataClasses/src/main/kotlin/dev/tokoi/Program.kt
@@ -4,11 +4,16 @@ fun main(args: Array<String>) {
     var wesley = Student(1, "Wesley")
     var tokoi = Student(1, "Wesley")
 
+    println(wesley)
+
     if (wesley == tokoi) {
         println("Equal")
     } else {
         println("Not equal")
     }
+
+    var newWesley = wesley.copy(name = "Robert")
+    println(newWesley)
 }
 
 data class Student(val id: Int, val name: String)
