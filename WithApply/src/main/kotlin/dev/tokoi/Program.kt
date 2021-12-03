b3087f56204c4178bff4d12ec7d0e8c15eca5e6d
Wesley Tokoi <tokoiwesley@gmail.com>
2021-12-03 16:41:30 +0300

    Add a 'with' example

---
+++ WithApply/src/main/kotlin/dev/tokoi/Program.kt
@@ -0,0 +1,22 @@
+package dev.tokoi
+
+import java.util.*
+
+class Meeting {
+    var Title: String = ""
+    var When: Date = Date()
+    var Who = mutableListOf<String>()
+}
+
+fun main(args: Array<String>) {
+    val m = Meeting()
+    m.Title = "Board Meeting"
+    m.When = Date(2017, 1, 1)
+    m.Who.add("Wesley")
+
+    with(m) {
+        m.Title = "Board Meeting"
+        m.When = Date(2017, 1, 1)
+        m.Who.add("Wesley")
+    }
+}
