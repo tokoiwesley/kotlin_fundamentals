564e8c71671ac08cfd246011f26bffcb0df8934d
Wesley Tokoi <tokoiwesley@gmail.com>
2021-12-03 13:24:52 +0300

    Use an anonymous class to implement functionality

---
+++ Functional/src/main/kotlin/dev /tokoi/Program.kt
@@ -0,0 +1,49 @@
+package dev.tokoi
+
+fun main(args: Array<String>) {
+    var program = Program()
+//    program.fibonacci(8)
+    program.fibonacci(8, object : Process {
+        override fun execute(value: Int) {
+            println(value)
+        }
+
+    })
+}
+
+interface Process {
+    fun execute(value: Int)
+}
+
+class Program {
+
+    fun fibonacci(limit: Int, action: Process) {
+        var prev = 0
+        var prevprev = 0
+        var current = 1
+
+        for (i: Int in 1..limit) {
+            action.execute(current)
+
+            var temp = current
+            prevprev = prev
+            prev = temp
+            current = prev + prevprev
+        }
+    }
+
+    fun fibonacci(limit: Int) {
+        var prev = 0
+        var prevprev = 0
+        var current = 1
+
+        for (i: Int in 1..limit) {
+            println(current)
+
+            var temp = current
+            prevprev = prev
+            prev = temp
+            current = prev + prevprev
+        }
+    }
+}
