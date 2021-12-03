6e3e92da3e72dc8bdd096d890f6b0cc4926dac01
Wesley Tokoi <tokoiwesley@gmail.com>
2021-12-03 20:24:47 +0300

    Use the 'all' method

---
+++ FilteringAndSorting/src/main/kotlin/dev/tokoi/Predicates.kt
@@ -0,0 +1,13 @@
+package dev.tokoi
+
+fun main(args: Array<String>) {
+    val ints = listOf(1, 2, 3, 4, 5)
+
+    var largeInts = ints.all { it > 6 }
+
+    println(largeInts)
+
+    largeInts = ints.all { it > 3 }
+
+    println(largeInts)
+}
