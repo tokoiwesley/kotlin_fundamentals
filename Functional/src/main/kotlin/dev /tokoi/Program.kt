342c325d9abbbae64c0de48ff19165959c3eee03
Wesley Tokoi <tokoiwesley@gmail.com>
2021-12-03 15:14:36 +0300

    Use a higher order function

--- Functional/src/main/kotlin/dev /tokoi/Program.kt
+++ Functional/src/main/kotlin/dev /tokoi/Program.kt
@@ -3,12 +3,13 @@ package dev.tokoi
 fun main(args: Array<String>) {
     var program = Program()
 //    program.fibonacci(8)
-    program.fibonacci(8, object : Process {
+    /*program.fibonacci(8, object : Process {
         override fun execute(value: Int) {
             println(value)
         }
 
-    })
+    })*/
+    program.fibonacci(8, {n -> println(n)})
 }
 
 interface Process {
@@ -16,6 +17,20 @@ interface Process {
 }
 
 class Program {
+    fun fibonacci(limit: Int, action: (Int) -> Unit) {
+        var prev = 0
+        var prevprev = 0
+        var current = 1
+
+        for (i: Int in 1..limit) {
+            action(current)
+
+            var temp = current
+            prevprev = prev
+            prev = temp
+            current = prev + prevprev
+        }
+    }
 
     fun fibonacci(limit: Int, action: Process) {
         var prev = 0
