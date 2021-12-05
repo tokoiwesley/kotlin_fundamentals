f0b6551e05c7570dba4a18a7f3ef2b664db57acb
Wesley Tokoi <tokoiwesley@gmail.com>
2021-12-05 09:55:11 +0300

    Use a sequence for better efficiency than a lists

--- FilteringAndSorting/src/main/kotlin/dev/tokoi/FilterMap.kt
+++ FilteringAndSorting/src/main/kotlin/dev/tokoi/FilterMap.kt
@@ -4,10 +4,15 @@ fun main(args: Array<String>) {
 
     val meetings = listOf(Meeting(1, "Board Meeting"), Meeting(2, "Committee Meeting"))
 
-    val titles = meetings
+    /*val titles = meetings
         .asSequence()
         .filter { println("filter($it)"); it.title.endsWith("g") }
-        .map { println("map($it)"); it.title }
+        .map { println("map($it)"); it.title }*/
 
-    for (t in titles) println(t)
+    val title = meetings
+        .asSequence()
+        .map { println("map($it)"); it.title.toUpperCase() }
+        .find { it.startsWith("BOARD") }
+
+    println(title)
 }
