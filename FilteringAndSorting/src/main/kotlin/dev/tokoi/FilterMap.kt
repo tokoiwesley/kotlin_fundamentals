64258ae97251b9cc8ca360c12e35d3b52241e772
Wesley Tokoi <tokoiwesley@gmail.com>
2021-12-05 09:47:53 +0300

    Use a sequence for lazy evaluation of items

--- FilteringAndSorting/src/main/kotlin/dev/tokoi/FilterMap.kt
+++ FilteringAndSorting/src/main/kotlin/dev/tokoi/FilterMap.kt
@@ -1,25 +1,13 @@
 package dev.tokoi
 
 fun main(args: Array<String>) {
-    val ints = listOf(1, 2, 3, 4, 5)
-
-    val smallInts = ints.filter { it < 4 }
-
-//    for (i: Int in smallInts) println(i)
-
-    var squaredInts = ints.map { it * it }
-
-//    for (i: Int in squaredInts) println(i)
-
-    var smallSquaredInts = ints.filter { it < 5 }
-        .map { it * it }
-
-//    for (i: Int in smallSquaredInts) println(i)
 
     val meetings = listOf(Meeting(1, "Board Meeting"), Meeting(2, "Committee Meeting"))
 
     val titles = meetings
-        .filter { it.title.startsWith("C") }
-        .map { m -> m.title }
+        .asSequence()
+        .filter { println("filter($it)"); it.title.endsWith("g") }
+        .map { println("map($it)"); it.title }
+
     for (t in titles) println(t)
 }
