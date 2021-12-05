7c32c86cbcc9d50b13e3d58bf9303ae9fcc0e2a7
Wesley Tokoi <tokoiwesley@gmail.com>
2021-12-05 09:59:55 +0300

    Capture and pass the iteration of a sequence to a function

--- FilteringAndSorting/src/main/kotlin/dev/tokoi/FilterMap.kt
+++ FilteringAndSorting/src/main/kotlin/dev/tokoi/FilterMap.kt
@@ -1,18 +1,24 @@
 package dev.tokoi
 
+fun iter(seq: Sequence<String>) {
+    for (t in seq) println(t)
+}
+
 fun main(args: Array<String>) {
 
     val meetings = listOf(Meeting(1, "Board Meeting"), Meeting(2, "Committee Meeting"))
 
-    /*val titles = meetings
+    val titles = meetings
         .asSequence()
         .filter { println("filter($it)"); it.title.endsWith("g") }
-        .map { println("map($it)"); it.title }*/
+        .map { println("map($it)"); it.title }
 
-    val title = meetings
+    iter(titles)
+
+    /*val title = meetings
         .asSequence()
         .map { println("map($it)"); it.title.toUpperCase() }
         .find { it.startsWith("BOARD") }
 
-    println(title)
+    println(title)*/
 }
