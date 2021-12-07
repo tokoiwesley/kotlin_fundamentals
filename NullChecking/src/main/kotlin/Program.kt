f4f0492e00b294d3893cbe2a4accb22addbaba8e
Wesley Tokoi <tokoiwesley@gmail.com>
2021-12-07 08:46:37 +0300

    Use nullable checks

---
+++ NullChecking/src/main/kotlin/Program.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+    var m: Meeting? = null
+    var newMeeting: Meeting = Meeting()
+
+//    newMeeting = m
+    closeMeeting(m)
+    closeMeeting(newMeeting)
+}
+
+fun closeMeeting(m: Meeting?): Boolean? {
+    return if (m?.canClose == true) m?.close()
+    else false
+}
+
+class Meeting() {
+    val canClose: Boolean = false
+
+    fun close(): Boolean {
+        return true
+    }
+}
