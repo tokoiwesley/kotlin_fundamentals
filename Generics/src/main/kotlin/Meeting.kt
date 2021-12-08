4abbcfa7d20557ddc737200aa08d0a7cc59d0735
Wesley Tokoi <tokoiwesley@gmail.com>
2021-12-09 00:26:17 +0300

    Modify reification with 'noinline'

--- Generics/src/main/kotlin/Meeting.kt
+++ Generics/src/main/kotlin/Meeting.kt
@@ -18,6 +18,16 @@ inline fun <reified T> List<*>.typeOf(): List<T> {
     return returnList
 }
 
+fun <T : Meeting> buildMeeting(meetingClass: Class<T>, action: (T) -> Unit): T {
+    val meeting: T = meetingClass.newInstance()
+    action(meeting)
+    return meeting
+}
+
+inline fun <reified T : Meeting> buildMeeting(noinline action: (T) -> Unit): T {
+    return buildMeeting(T::class.java, action)
+}
+
 open class Meeting {
 }
 
